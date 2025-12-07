7ad83366c772702339ade41f7a3a8e270abf1174
mohamed <77855298+mohamed7-dev@users.noreply.github.com>
2025-12-07 20:28:53 +0200

    chore: refactored some root project settings

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,7 +1,7 @@
 import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
-import tseslint from "typescript-eslint";
 import onlyWarn from "eslint-plugin-only-warn";
+import tseslint from "typescript-eslint";
 
 /**
  * A shared ESLint configuration for the repository.
